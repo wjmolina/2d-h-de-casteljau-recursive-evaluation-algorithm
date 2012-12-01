aeb97d2f1099b84c6114f1ba2eb8e980742cce4f
molinaw1 <w33jjj@gmail.com>
2012-12-01 09:39:56 -0600

    Update HeaderFiles/HelperFunctions.h

--- HeaderFiles/HelperFunctions.h
+++ HeaderFiles/HelperFunctions.h
@@ -11,8 +11,8 @@
 
 bool LessThanOrEqualTo(double, double);	// Compares Doubles
 double alpha(int, int, double, double);	// Theoretical Function
-double prod(int, int, double, double);		// Particular Product Notation
-int fact(int);										// Factorial
-int choo(int, int);								// n Choose k
+double prod(int, int, double, double);	// Particular Product Notation
+int fact(int);				  		    				// Factorial
+int choo(int, int);			    		  			// n Choose k
 
 #endif // HELPERFUNCTIONS_H
