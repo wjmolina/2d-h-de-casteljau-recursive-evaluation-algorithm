613dbb2c8c72cb1577df88b6d2e16cf7ab5b792a
molinaw1 <w33jjj@gmail.com>
2012-12-01 09:41:21 -0600

    Update HeaderFiles/HelperFunctions.h

--- HeaderFiles/HelperFunctions.h
+++ HeaderFiles/HelperFunctions.h
@@ -9,10 +9,10 @@
 
 #define EPSILON 0.0001
 
-bool LessThanOrEqualTo(double, double);	// Compares Doubles
-double alpha(int, int, double, double);	// Theoretical Function
-double prod(int, int, double, double);	// Particular Product Notation
-int fact(int);				  		    				// Factorial
-int choo(int, int);			    		  			// n Choose k
+bool LessThanOrEqualTo(double, double); // Compares Doubles
+double alpha(int, int, double, double); // Theoretical Function
+double prod(int, int, double, double);  // Particular Product Notation
+int fact(int);                          // Factorial
+int choo(int, int);                     // n Choose k
 
 #endif // HELPERFUNCTIONS_H
